1d63e0e3d5c6f267e14b5c630d384657f530b222
최수빈 <83692497+ssbinn@users.noreply.github.com>
2022-05-06 22:06:15 +0900

    Create 2525.js

---
+++ 백준/2525.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const filePath = process.platform === "window" ? "/dev/stdin" : "input.txt";
+
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+let h = input[0].split(' ')[0];
+let m = input[0].split(' ')[1];
+let t = input[1];
+
+function solution(h, m, t) {
+    m += t;
+
+    while (m >= 60) {
+        m -= 60;
+        h += 1;
+    }
+    h %= 24;
+    console.log(h, m)
+}
+// solution(h, m, t)
+solution(+h, +m, +t) // + 연산자를 붙여야 결과가 나오는 이유?
