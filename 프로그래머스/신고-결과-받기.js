71422ef8b290c46ddc03f2cf168047d3747e8ea7
최수빈 <83692497+ssbinn@users.noreply.github.com>
2022-06-24 12:57:59 +0900

    Update 신고-결과-받기.js

--- 프로그래머스/신고-결과-받기.js
+++ 프로그래머스/신고-결과-받기.js
@@ -3,6 +3,7 @@ function solution(id_list, report, k) {
     const list = {};
     const answer = new Array(id_list.length).fill(0);
     
+    // map으로 배열의 요소에 접근
     id_list.map((id) => {
         list[id] = [];  // { muzi: [], ...}
     })
@@ -16,7 +17,7 @@ function solution(id_list, report, k) {
         // {frodo: [ 'muzi', 'apeach' ]}
     })
     
-    for (const item in list) {
+    for (const item in list) {  // for...in문으로 index에 접근, console.log(item); // 0 1 이런 식
         if(list[item].length >= k) {
             list[item].map((user) => {
                 answer[id_list.indexOf(user)] += 1;
