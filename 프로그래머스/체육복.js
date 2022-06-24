2eea6a77fa0908b3602394fa3793ac9d79c95053
최수빈 <83692497+ssbinn@users.noreply.github.com>
2022-06-24 17:34:49 +0900

    Update 체육복.js

--- 프로그래머스/체육복.js
+++ 프로그래머스/체육복.js
@@ -3,9 +3,10 @@ function solution(n, lost, reserve) {
     
     // 여벌 체육복을 가져온 학생이 체육복을 도난당한 경우, 중복 값 제거
     let realLost = lost.filter((l) => !reserve.includes(l));
-    let realReserve = reserve.filter((r) => !lost.includes(r));
+    let realReserve = reserve.filter((r) => !lost.includes(r));  // lost.includes()는 표현식이기 때문에 중괄호로 감싸지 않음
     answer += lost.length - realLost.length;
     
+    // lost 배열 요소들이 내림차순일 경우를 대비해 오름차순으로 정렬
     realLost.sort((a ,b) => {return a - b});
     
     realLost.forEach((item) => {
@@ -23,3 +24,9 @@ function solution(n, lost, reserve) {
     })
     return answer;
 }
+
+/*
+(param1, param2, …, paramN) => { statements }
+(param1, param2, …, paramN) => expression
+// 다음과 동일함:  => { return expression; }
+*/
